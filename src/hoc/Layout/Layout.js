3a9c21a10c386c3f782c476bad4e0895dbb7eb6e
andrey-dum <duminskiy@gmail.com>
2020-12-14 17:12:20 +0200

    Layout

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classes from './Layout.module.css'
+
+const Layout = (props) => {
+    return (
+        <div className={classes.Layout}>
+            <main>
+                {props.children}
+            </main>
+        </div>
+    );
+}
+
+export default Layout;
