bee0e113db977e982fb4a0b315c01845bcd6c5e9
andrey-dum <duminskiy@gmail.com>
2020-12-19 18:17:33 +0200

    redux, qyuzReducer, actions for QuizList.js

--- src/containers/QuizList/QuizList.js
+++ src/containers/QuizList/QuizList.js
@@ -1,32 +1,41 @@
 import React from 'react'
-import axios from '../../axios/axios-quiz'
 import { NavLink } from 'react-router-dom'
+
+import { useDispatch, useSelector } from 'react-redux'
 
 import './QuizList.css'
 import Loader from '../../components/UI/Loader/Loader'
+import { fetchQuizes } from '../../store/actions/quiz'
 
 export default function QuizList() {
 
-    const [quizes, setQuizes] = React.useState([])
-    const [loading, setLoading] = React.useState(true)
+    // const [quizes, setQuizes] = React.useState([])
+    // const [loading, setLoading] = React.useState(true)
+
+    const dispatch = useDispatch()
+    const state = useSelector(state => state.quiz)
 
     React.useEffect(() => {
-        axios.get('/quizes.json')
-        .then(response => {
-            const quizList = []
-            Object.keys(response.data).forEach((key, index) => {
-                quizList.push({
-                        id: key,
-                        name: `Test #${index + 1}`
-                    })
-            })
+        dispatch(fetchQuizes())
+        // axios.get('/quizes.json')
+        // .then(response => {
+        //     const quizList = []
+        //     Object.keys(response.data).forEach((key, index) => {
+        //         quizList.push({
+        //                 id: key,
+        //                 name: `Test #${index + 1}`
+        //             })
+        //     })
 
-            setQuizes(quizList)
-            setLoading(false)
-        })
-        .catch(e => console.log(e)) 
+        //     setQuizes(quizList)
+        //     setLoading(false)
+        // })
+        // .catch(e => console.log(e)) 
 
-    }, [])
+    }, [dispatch])
+
+    const quizes = state.quizes
+    const loading = state.loading
 
     const renderQuizes = () => {
         return quizes.map((quiz) => (
@@ -46,7 +55,7 @@ export default function QuizList() {
         <div className="QuizList">
             <div className="QuizList__block">
                 <h1>Список тестов</h1>
-                { loading 
+                { loading && quizes.length !== 0
                     ? <Loader />
                     : <ul>
                             { renderQuizes() }
